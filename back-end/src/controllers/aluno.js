255e00b759ec7936bf0c709e15a363515cf6f57b
LAUANY BARBOSA GONCALVES <1091392223020@fatec-srv.local>
2023-10-02 20:24:45 -0300

    (02/10) Inserção de relacionamento muitos-muitos

--- back-end/src/controllers/aluno.js
+++ back-end/src/controllers/aluno.js
@@ -4,6 +4,7 @@ const controller = {}   // Objeto vazio
 
 controller.create = async function(req, res) {
   try {
+
     // Conecta-se ao BD e envia uma instrução
     // de criação de um novo documento, com os
     // dados que estão dentro de req.body
@@ -23,13 +24,19 @@ controller.create = async function(req, res) {
 }
 
 controller.retrieveAll = async function(req, res) {
+
+    // Por padrão, não inclui nenhum relacionamento
+    const include = {}
+
+    if(req.query.turmas) include.turmas = true
+
     try {
       // Manda buscar os dados do servidor
-      // Traz ordenado por nome, depois por nivel
+
       const result = await prisma.aluno.findMany({
+        include,
         orderBy: [
-            { nome: 'asc' },     //ordem ascendente
-            { cpf: 'asc' }     //ordem ascendente
+            { nome: 'asc' }     //ordem ascendente
         ]
       })
       // HTTP 200: Ok
@@ -65,6 +72,7 @@ controller.retrieveOne = async function(req, res) {
 }
 
 controller.update = async function(req, res) {
+
     try {
         const result = await prisma.aluno.update({
             where: { id: req.params.id },
@@ -89,6 +97,42 @@ controller.delete = async function(req, res) {
     try {
         const result = await prisma.aluno.delete({
             where: { id: req.params.id },
+        })
+
+        // Encontrou => retorna HTTP 204: No content
+        if(result) res.send(204).end()
+
+        //Não encontrou (e não atualizou) => retorna HTTP 404: Not found
+        else res.status(404).end()
+    }
+    catch(error) {
+        // Deu errado: exibe o erro no console do back-end
+        console.error(error)
+        // Envia o erro ao front-end, com status 500
+        // HTTP 500: Internal Server Error
+        res.status(500).send(error)
+    }
+}
+
+controller.addTurma = async function(req, res) {
+    try {
+
+        // Busca o aluno para recuperar a lista de ids de turmas dele
+        const aluno = await prisma.aluno.findUnique({
+            where: { id: req.params.alunoId }
+        })
+
+        //Se ele não tiver turmas ainda, criamos a lista vazia
+        const turmaIds = aluno.turmaIds || []
+
+        //Se o id de turma passado ainda não estiver na lista do aluno, fazemos a respectiva inserção 
+        if(! turmaIds.include(req.params.turmaId))
+            turmaIds.push(req.params.turmaId)
+
+        // Atualizamos o aluno com uma lista de ids de turma atualizada
+        const result = await prisma.aluno.update({
+            where: { id: req.params.alunoId },
+            data: { turmaIds }
         })
 
         // Encontrou => retorna HTTP 204: No content
